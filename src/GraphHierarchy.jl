228c1ad067191bb3c43acaf44f909d62dc07564b
Giannis Moutsinas <giannis.moutsinas@gmail.com>
2019-11-14 13:42:10 +0000

    added support for BigFloat

--- src/GraphHierarchy.jl
+++ src/GraphHierarchy.jl
@@ -19,20 +19,49 @@ function calculationAssumingTransposedMatrix(A::SparseMatrixCSC{T1,T2} where T1 
 end
 
 
-function graphHierarchicalStructure(g::SimpleDiGraph{T} where T <: Int)
-    return calculationAssumingTransposedMatrix(sparse(transpose(adjacency_matrix(g))))
+function calculationAssumingTransposedMatrixBig(A::SparseMatrixCSC{T1,T2} where T1 <: Number where T2 <: Int)
+    d = vec(sum(A, dims=2))
+    db = [BigFloat(i) for i in d]
+    M = spdiagm(0 => d)-A
+    dropzeros!(M)
+    (x, y, v) = findnz(M)
+    v = [BigFloat(i) for i in v]
+    Mb = sparse(x, y, v, length(d), length(d))
+    HLs = lsqr(Mb,db)
+    pos = findall(!iszero,A)
+    HDs = sparse([p[2] for p in pos], [p[1] for p in pos], [HLs[p[1]] - HLs[p[2]] for p in pos], length(d), length(d))
+    HDsums = sum(HDs, dims=1)
+    ICs = [d[i] == 0 ? BigFloat(1) : BigFloat(1) - HDsums[i]/d[i] for i in 1:length(d)]
+    return (HLs, ICs, HDs)
 end
 
-function graphHierarchicalStructure(g::SimpleGraph{T} where T <: Int)
-    return calculationAssumingTransposedMatrix(adjacency_matrix(g))
+
+function graphHierarchicalStructure(g::SimpleDiGraph{T} where T <: Int, big::Bool=false)
+    if big
+        return calculationAssumingTransposedMatrixBig(sparse(transpose(adjacency_matrix(g))))
+    else
+        return calculationAssumingTransposedMatrix(sparse(transpose(adjacency_matrix(g))))
+    end
 end
 
-function graphHierarchicalStructure(A::SparseMatrixCSC{T1,T2} where T1 <: Number where T2 <: Int)
-    return calculationAssumingTransposedMatrix(sparse(transpose(A)))
+function graphHierarchicalStructure(g::SimpleGraph{T} where T <: Int, big::Bool=false)
+    if big
+        return calculationAssumingTransposedMatrixBig(adjacency_matrix(g))
+    else
+        return calculationAssumingTransposedMatrix(adjacency_matrix(g))
+    end
+end
+
+function graphHierarchicalStructure(A::SparseMatrixCSC{T1,T2} where T1 <: Number where T2 <: Int, big::Bool=false)
+    if big
+        return calculationAssumingTransposedMatrixBig(sparse(transpose(A)))
+    else
+        return calculationAssumingTransposedMatrix(sparse(transpose(A)))
+    end
 end
 
 function graphHierarchicalCoefficients(diffs::SparseMatrixCSC{T1,T2} where T1 <: AbstractFloat where T2 <: Int )
-    diffarray = findnz(diffs)[3] #[diffs[p] for p in findall(!iszero, diffs)]
+    diffarray = findnz(diffs)[3]
     av = mean(diffarray)
     stdev = std(diffarray, mean=av)
     return (1 - av, stdev)
