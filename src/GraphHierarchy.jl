fff60c364e1c4b29676f08be69422bf0e5ab5016
Giannis Moutsinas <giannis.moutsinas@gmail.com>
2020-01-16 23:00:01 +0000

    backward hierarchy added, more tests needed

--- src/GraphHierarchy.jl
+++ src/GraphHierarchy.jl
@@ -1,12 +1,19 @@
 module GraphHierarchy
 
 using LightGraphs
+using SimpleWeightedGraphs
 using LinearAlgebra
 using SparseArrays
 using IterativeSolvers
+using StatsBase
 using Statistics
 
-function calculationAssumingTransposedMatrix(A::SparseMatrixCSC{T1,T2} where T1 <: Number where T2 <: Int)
+export forward_hierarchical_structure
+export backward_hierarchical_structure
+export hierarchical_structure
+export hierarchical_coefficients
+
+function calculatingStructureAssumingTransposedMatrix(A::SparseMatrixCSC{T1,T2} where T1 <: Number where T2 <: Int)
     d = vec(sum(A, dims=2))
     M = spdiagm(0 => d)-A
     dropzeros!(M)
@@ -14,65 +21,223 @@ function calculationAssumingTransposedMatrix(A::SparseMatrixCSC{T1,T2} where T1 
     pos = findall(!iszero,A)
     HDs = sparse([p[2] for p in pos], [p[1] for p in pos], [HLs[p[1]] - HLs[p[2]] for p in pos], length(d), length(d))
     HDsums = sum(HDs, dims=1)
-    ICs = [d[i] == 0 ? 1.0 : 1.0 - HDsums[i]/d[i] for i in 1:length(d)]
-    return (HLs, ICs, HDs)
+    ICs = [d[i] == 0 ? 1.0 : 1.0 - sum(HDs[:,i])/d[i] for i in 1:length(d)]
+    return (HLs, ICs, HDs )
 end
 
+function calculatingStructureAssumingTransposedMatrixBig(A::SparseMatrixCSC{T1,T2} where T1 <: Number where T2 <: Int)
+    (x, y, v) = findnz(A)
+    vb = -map(BigFloat,v)
+    M = sparse(x, y, vb, size(A)[1], size(A)[1])
+    d = -vec(sum(M, dims=2))
+    for i in 1:length(d)
+        if d[i] > 0
+            M[i,i] = d[i]
+        end
+    end
+    HLs = lsqr(M,d)
+    HDs = sparse(x, y, [ HLs[x[i]] - HLs[y[i]] for i in 1:length(x) ], length(d), length(d) )
+    ICs = [d[i] > 0 ? BigFloat(1) - sum(HDs[i,:])/d[i] : BigFloat(1) for i in 1:length(d)]
+    return (HLs, ICs, HDs )
+end
 
-function calculationAssumingTransposedMatrixBig(A::SparseMatrixCSC{T1,T2} where T1 <: Number where T2 <: Int)
+function calculatingStructureAssumingTransposedWeightedMatrix(A::SparseMatrixCSC{T1,T2} where T1 <: Number where T2 <: Int)
+    (x, y, v) = findnz(A)
     d = vec(sum(A, dims=2))
-    db = [BigFloat(i) for i in d]
     M = spdiagm(0 => d)-A
     dropzeros!(M)
-    (x, y, v) = findnz(M)
-    vb = [BigFloat(i) for i in v]
-    Mb = sparse(x, y, vb, length(d), length(d))
-    HLs = lsqr(Mb,db)
-    pos = findall(!iszero,A)
-    HDs = sparse([p[2] for p in pos], [p[1] for p in pos], [HLs[p[1]] - HLs[p[2]] for p in pos], length(d), length(d))
+    HLs = lsqr(M,d)
+    HDs = sparse(x, y, [ HLs[x[i]] - HLs[y[i]] for i in 1:length(x) ], length(d), length(d) )
     HDsums = sum(HDs, dims=1)
-    ICs = [d[i] == 0 ? BigFloat(1) : BigFloat(1) - HDsums[i]/d[i] for i in 1:length(d)]
-    return (HLs, ICs, HDs)
+    ICs = [d[i] > 0 ? 1.0 - A[i,:]'*HDs[i,:]/d[i] : 1.0 for i in 1:length(d)]
+    return (HLs, ICs, sparse(transpose(HDs)) )
 end
 
+function calculatingStructureAssumingTransposedWeightedMatrixBig(A::SparseMatrixCSC{T1,T2} where T1 <: Number where T2 <: Int)
+    (x, y, v) = findnz(A)
+    vb = -map(BigFloat,v)
+    M = sparse(x, y, vb, size(A)[1], size(A)[1])
+    d = -vec(sum(M, dims=2))
+    for i in 1:length(d)
+        if d[i] > 0
+            M[i,i] = d[i]
+        end
+    end
+    HLs = lsqr(M,d)
+    HDs = sparse(x, y, [ HLs[x[i]] - HLs[y[i]] for i in 1:length(x) ], length(d), length(d) )
+    HDsums = sum(HDs, dims=1)
+    ICs = [d[i] > 0 ? BigFloat(1) - A[i,:]'*HDs[i,:]/d[i] : BigFloat(1) for i in 1:length(d)]
+    return (HLs, ICs, HDs )
+end
 
-function graphHierarchicalStructure(g::SimpleDiGraph{T} where T <: Int, big::Bool=false)
+function forward_hierarchical_structure(g::SimpleDiGraph{T} where T <: Int; big::Bool=false)
     if big
-        return calculationAssumingTransposedMatrixBig(sparse(transpose(adjacency_matrix(g))))
+        return calculatingStructureAssumingTransposedMatrixBig(sparse(transpose(adjacency_matrix(g))))
     else
-        return calculationAssumingTransposedMatrix(sparse(transpose(adjacency_matrix(g))))
+        return calculatingStructureAssumingTransposedMatrix(sparse(transpose(adjacency_matrix(g))))
     end
 end
 
-function graphHierarchicalStructure(g::SimpleGraph{T} where T <: Int, big::Bool=false)
+function backward_hierarchical_structure(g::SimpleDiGraph{T} where T <: Int; big::Bool=false)
     if big
-        return calculationAssumingTransposedMatrixBig(adjacency_matrix(g))
+        return calculatingStructureAssumingTransposedMatrixBig(adjacency_matrix(g))
     else
-        return calculationAssumingTransposedMatrix(adjacency_matrix(g))
+        return calculatingStructureAssumingTransposedMatrix(adjacency_matrix(g))
     end
 end
 
-function graphHierarchicalStructure(A::SparseMatrixCSC{T1,T2} where T1 <: Number where T2 <: Int, big::Bool=false)
-    if big
-        return calculationAssumingTransposedMatrixBig(sparse(transpose(A)))
+function hierarchical_structure(g::SimpleDiGraph{T} where T <: Int; big::Bool=false)
+    return (forward_hierarchical_structure(g, big = big),backward_hierarchical_structure(g, big = big))
+end
+
+function forward_hierarchical_structure(g::SimpleWeightedDiGraph{T1,T2} where T1 <: Int where T2 <: Number; big::Bool=false, weighted::Bool=true)
+    if weighted
+        if big
+            return calculatingStructureAssumingTransposedWeightedMatrixBig(sparse(transpose(LightGraphs.weights(g))))
+        else
+            return calculatingStructureAssumingTransposedWeightedMatrix(sparse(transpose(LightGraphs.weights(g))))
+        end
     else
-        return calculationAssumingTransposedMatrix(sparse(transpose(A)))
+        if big
+            return calculatingStructureAssumingTransposedMatrixBig(sparse(transpose(adjacency_matrix(g))))
+        else
+            return calculatingStructureAssumingTransposedMatrix(sparse(transpose(adjacency_matrix(g))))
+        end
     end
 end
 
-function graphHierarchicalCoefficients(diffs::SparseMatrixCSC{T1,T2} where T1 <: AbstractFloat where T2 <: Int )
+function backward_hierarchical_structure(g::SimpleWeightedDiGraph{T1,T2} where T1 <: Int where T2 <: Number; big::Bool=false, weighted::Bool=true)
+    if weighted
+        if big
+            return calculatingStructureAssumingTransposedWeightedMatrixBig(sparse(LightGraphs.weights(g)))
+        else
+            return calculatingStructureAssumingTransposedWeightedMatrix(sparse(LightGraphs.weights(g)))
+        end
+    else
+        if big
+            return calculatingStructureAssumingTransposedMatrixBig(adjacency_matrix(g))
+        else
+            return calculatingStructureAssumingTransposedMatrix(adjacency_matrix(g))
+        end
+    end
+end
+
+function hierarchical_structure(g::SimpleWeightedDiGraph{T1,T2} where T1 <: Int where T2 <: Number; big::Bool=false, weighted::Bool=true)
+    return (forward_hierarchical_structure(g, big = big, weighted = weighted),backward_hierarchical_structure(g, big = big, weighted = weighted))
+end
+
+function forward_hierarchical_structure(g::SimpleGraph{T} where T <: Int; big::Bool=false)
+    if big
+        return calculatingStructureAssumingTransposedMatrixBig(adjacency_matrix(g))
+    else
+        return calculatingStructureAssumingTransposedMatrix(adjacency_matrix(g))
+    end
+end
+
+function backward_hierarchical_structure(g::SimpleGraph{T} where T <: Int; big::Bool=false)
+    forward_hierarchical_structure(g, big = big)
+end
+
+function hierarchical_structure(g::SimpleGraph{T} where T <: Int; big::Bool=false)
+    return forward_hierarchical_structure(g, big = big)
+end
+
+function forward_hierarchical_structure(g::SimpleWeightedGraph{T1,T2} where T1 <: Int where T2 <: Number; big::Bool=false, weighted::Bool=true)
+    if weighted
+        if big
+            return calculatingStructureAssumingTransposedWeightedMatrixBig(sparse(LightGraphs.weights(g)))
+        else
+            return calculatingStructureAssumingTransposedWeightedMatrix(sparse(LightGraphs.weights(g)))
+        end
+    else
+        if big
+            return calculatingStructureAssumingTransposedMatrixBig(adjacency_matrix(g))
+        else
+            return calculatingStructureAssumingTransposedMatrix(adjacency_matrix(g))
+        end
+    end
+end
+
+function backward_hierarchical_structure(g::SimpleWeightedGraph{T1,T2} where T1 <: Int where T2 <: Number; big::Bool=false, weighted::Bool=true)
+    return forward_hierarchical_structure(g, big = big, weighted = weighted)
+end
+
+function hierarchical_structure(g::SimpleWeightedGraph{T1,T2} where T1 <: Int where T2 <: Number; big::Bool=false, weighted::Bool=true)
+    return forward_hierarchical_structure(g, big = big, weighted = weighted)
+end
+
+function forward_hierarchical_structure(A::SparseMatrixCSC{T1,T2} where T1 <: Number where T2 <: Int; big::Bool=false)
+    if big
+        return calculatingStructureAssumingTransposedMatrixBig(sparse(transpose(A)))
+    else
+        return calculatingStructureAssumingTransposedMatrix(sparse(transpose(A)))
+    end
+end
+
+function backward_hierarchical_structure(A::SparseMatrixCSC{T1,T2} where T1 <: Number where T2 <: Int; big::Bool=false)
+    if big
+        return calculatingStructureAssumingTransposedMatrixBig(A)
+    else
+        return calculatingStructureAssumingTransposedMatrix(A)
+    end
+end
+
+function hierarchical_structure(A::SparseMatrixCSC{T1,T2} where T1 <: Number where T2 <: Int; big::Bool=false)
+    return ( forward_hierarchical_structure(A, big=big), backward_hierarchical_structure(A, big=big) )
+end
+
+function hierarchical_coefficients(g::Union{SimpleGraph{T},SimpleDiGraph{T}} where T <: Int, diffs::SparseMatrixCSC{T1,T2} where T1 <: AbstractFloat where T2 <: Int)
     diffarray = findnz(diffs)[3]
     av = mean(diffarray)
-    stdev = std(diffarray, mean=av)
-    return (1 - av, stdev)
+    return (1 - av, std(diffarray, mean=av, corrected=false))
 end
 
-function graphHierarchicalCoefficients(hstruc::Tuple{Array{T1,1},Array{T1,1},SparseMatrixCSC{T1,T2}} where T1 <: AbstractFloat where T2 <: Int)
-    return graphHierarchicalCoefficients(hstruc[3])
+function hierarchical_coefficients(g::Union{SimpleWeightedGraph{T1,T2},SimpleWeightedDiGraph{T1,T2}} where T1 <: Int where T2 <: AbstractFloat, diffs::SparseMatrixCSC{T1,T2} where T1 <: AbstractFloat where T2 <: Int; weighted::Bool = true )
+    if weighted
+        W0 = sparse(LightGraphs.weights(g))
+        (xw,yw,vw) = findnz(W0)
+        (x,y,v) = findnz(diffs)
+
+        if sum(map(abs,x - xw)) + sum(map(abs,y - yw)) > 0
+            W0 = sparse(transpose(LightGraphs.weights(g)))
+            (xw,yw,vw) = findnz(W0)
+        end
+
+        if sum(map(abs,x - xw)) + sum(map(abs,y - yw)) > 0
+            error("hierarchical differences do not match the weighted adjacency matrix:\n", diffs, "\n", W0)
+        end
+
+        w0 = [ W0[x[i],y[i]] for i in 1:length(x)]
+        difftype = typeof(v[1])
+        if difftype == typeof(w0[1])
+            av0 = mean(v, StatsBase.weights(w0))
+            return ( 1 - av0 ,std( v, StatsBase.weights(w0), mean=av0, corrected=false))
+        else
+            w = [ convert( difftype, c) for c in w0 ]
+            av = mean(v, StatsBase.weights(w))
+            return ( 1 - av ,std( v, StatsBase.weights(w), mean=av, corrected=false))
+        end
+    else
+        v = findnz(diffs)[3]
+        av = mean(v)
+        return (1 - av, std(v, mean=av))
+    end
 end
 
-export graphHierarchicalStructure
+function hierarchical_coefficients(g::Union{SimpleGraph{T},SimpleDiGraph{T}} where T <: Int, hstruc::Tuple{Array{T1,1},Array{T1,1},SparseMatrixCSC{T1,T2}} where T1 <: AbstractFloat where T2 <: Int )
+    return hierarchical_coefficients(g, hstruc[3])
+end
 
-export graphHierarchicalCoefficients
+function hierarchical_coefficients(g::Union{SimpleWeightedGraph{T1,T2},SimpleWeightedDiGraph{T1,T2}} where T1 <: Int  where T2 <: AbstractFloat, hstruc::Tuple{Array{T1,1},Array{T1,1},SparseMatrixCSC{T1,T2}} where T1 <: AbstractFloat where T2 <: Int;  weighted::Bool = true  )
+    return hierarchical_coefficients(g, hstruc[3], weighted=weighted)
+end
+
+function hierarchical_coefficients(g::Union{SimpleGraph{T},SimpleDiGraph{T}} where T <: Int , hstruc::Tuple{Tuple{Array{T1,1},Array{T1,1},SparseMatrixCSC{T1,T2}},Tuple{Array{T1,1},Array{T1,1},SparseMatrixCSC{T1,T2}}} where T1 <: AbstractFloat where T2 <: Int )
+    return (hierarchical_coefficients(g, hstruc[1]),hierarchical_coefficients(g, hstruc[2]))
+end
+
+function hierarchical_coefficients(g::Union{SimpleWeightedGraph{T1,T2},SimpleWeightedDiGraph{T1,T2}} where T1 <: Int  where T2 <: AbstractFloat , hstruc::Tuple{Tuple{Array{T1,1},Array{T1,1},SparseMatrixCSC{T1,T2}},Tuple{Array{T1,1},Array{T1,1},SparseMatrixCSC{T1,T2}}} where T1 <: AbstractFloat where T2 <: Int; weighted::Bool = true  )
+    return (hierarchical_coefficients(g, hstruc[1], weighted=weighted),hierarchical_coefficients(g, hstruc[2], weighted=weighted))
+end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,6 @@
 using LinearAlgebra
 using LightGraphs
+using SimpleWeightedGraphs
 using GraphHierarchy
 using SparseArrays
 using Test
@@ -11,9 +12,9 @@ using Test
     add_edge!(g,3,4)
     add_edge!(g,4,5)
     add_edge!(g,5,6)
-    HS = graphHierarchicalStructure(g)
+    HS = forward_hierarchical_structure(g)
     spdata = findnz(HS[3])
-    coeffs = graphHierarchicalCoefficients(HS)
+    coeffs = hierarchical_coefficients(g,HS)
     @test norm(HS[1] - [-2.5,-1.5,-0.5,0.5,1.5,2.5]) < 1.0e-15
     @test norm(HS[2] - [1,0,0,0,0,0]) < 1.0e-15
     @test norm(spdata[1] - [1,2,3,4,5]) < 1.0e-15
@@ -21,9 +22,9 @@ using Test
     @test norm(spdata[3]- [1.0,1.0,1.0,1.0,1.0]) < 1.0e-15
     @test abs(coeffs[1]) < 1.0e-15
     @test abs(coeffs[2]) < 1.0e-15
-    HSA = graphHierarchicalStructure(adjacency_matrix(g))
+    HSA = forward_hierarchical_structure(adjacency_matrix(g))
     spdataA = findnz(HSA[3])
-    coeffsA = graphHierarchicalCoefficients(HSA)
+    coeffsA = hierarchical_coefficients(g,HSA)
     @test norm(HSA[1] - HS[1]) < 1.0e-15
     @test norm(HSA[2] - HS[2]) < 1.0e-15
     @test norm(spdataA[1] - spdata[1]) < 1.0e-15
@@ -31,6 +32,34 @@ using Test
     @test norm(spdataA[3] - spdata[3]) < 1.0e-15
     @test abs(coeffsA[1] - coeffs[1]) < 1.0e-15
     @test abs(coeffsA[2] - coeffs[2]) < 1.0e-15
+    gr = SimpleDiGraph(transpose(adjacency_matrix(g)))
+    HSf = hierarchical_structure(g)
+    HSr = hierarchical_structure(gr)
+    spdata_f1 = findnz(HSf[1][3])
+    spdata_f2 = findnz(HSf[2][3])
+    coeffs_f = hierarchical_coefficients(g,HSf)
+    spdata_r1 = findnz(HSr[1][3])
+    spdata_r2 = findnz(HSr[2][3])
+    coeffs_r = hierarchical_coefficients(gr,HSr)
+    @test norm(HSf[1][1] - HSr[2][1]) < 1.0e-15
+    @test norm(HSf[2][1] - HSr[1][1]) < 1.0e-15
+    @test norm(HSf[1][2] - HSr[2][2]) < 1.0e-15
+    @test norm(HSf[2][2] - HSr[1][2]) < 1.0e-15
+    @test norm(spdata_f1[1] - spdata_r2[1]) < 1.0e-15
+    @test norm(spdata_f1[2] - spdata_r2[2]) < 1.0e-15
+    @test norm(spdata_f1[3] - spdata_r2[3]) < 1.0e-15
+    @test norm(spdata_f2[1] - spdata_r1[1]) < 1.0e-15
+    @test norm(spdata_f2[2] - spdata_r1[2]) < 1.0e-15
+    @test norm(spdata_f2[3] - spdata_r1[3]) < 1.0e-15
+    coeffs_f = hierarchical_coefficients(g,HSf)
+    coeffs_b = hierarchical_coefficients(gr,HSr)
+    @test abs(coeffs_f[1][1] - coeffs_b[2][1]) < 1.0e-15
+    @test abs(coeffs_f[1][2] - coeffs_b[2][2]) < 1.0e-15
+    @test abs(coeffs_f[2][1] - coeffs_b[1][1]) < 1.0e-15
+    @test abs(coeffs_f[2][2] - coeffs_b[1][2]) < 1.0e-15
+
+
+
 
     g = DiGraph(11)
     add_edge!(g,1,3)
@@ -51,9 +80,9 @@ using Test
     add_edge!(g,8,10)
     add_edge!(g,9,11)
     add_edge!(g,10,11)
-    HS = graphHierarchicalStructure(g)
+    HS = forward_hierarchical_structure(g)
     spdata = findnz(HS[3])
-    coeffs = graphHierarchicalCoefficients(HS)
+    coeffs = hierarchical_coefficients(g,HS)
     @test norm(HS[1] - [ceil(i/2) - 36.0/11.0 for i in 1:11]) < 1.0e-15
     @test norm(HS[2] - [1,1,0,0,0,0,0,0,0,0,0]) < 1.0e-15
     @test norm(spdata[1] - [1,2,1,2,3,4,3,4,5,6,5,6,7,8,7,8,9,10]) < 1.0e-15
@@ -61,9 +90,9 @@ using Test
     @test norm(spdata[3] - [1.0 for i in 1:length(spdata[3])]) < 1.0e-15
     @test abs(coeffs[1]-0.0) < 1.0e-15
     @test abs(coeffs[2]-0.0) < 1.0e-15
-    HSA = graphHierarchicalStructure(adjacency_matrix(g))
+    HSA = forward_hierarchical_structure(adjacency_matrix(g))
     spdataA = findnz(HSA[3])
-    coeffsA = graphHierarchicalCoefficients(HSA)
+    coeffsA = hierarchical_coefficients(g,HSA)
     @test norm(HSA[1] - HS[1]) < 1.0e-15
     @test norm(HSA[2] - HS[2]) < 1.0e-15
     @test norm(spdataA[1] - spdata[1]) < 1.0e-15
@@ -71,6 +100,31 @@ using Test
     @test norm(spdataA[3] - spdata[3]) < 1.0e-15
     @test abs(coeffsA[1] - coeffs[1]) < 1.0e-15
     @test abs(coeffsA[2] - coeffs[2]) < 1.0e-15
+    gr = SimpleDiGraph(transpose(adjacency_matrix(g)))
+    HSf = hierarchical_structure(g)
+    HSr = hierarchical_structure(gr)
+    spdata_f1 = findnz(HSf[1][3])
+    spdata_f2 = findnz(HSf[2][3])
+    coeffs_f = hierarchical_coefficients(g,HSf)
+    spdata_r1 = findnz(HSr[1][3])
+    spdata_r2 = findnz(HSr[2][3])
+    coeffs_r = hierarchical_coefficients(gr,HSr)
+    @test norm(HSf[1][1] - HSr[2][1]) < 1.0e-15
+    @test norm(HSf[2][1] - HSr[1][1]) < 1.0e-15
+    @test norm(HSf[1][2] - HSr[2][2]) < 1.0e-15
+    @test norm(HSf[2][2] - HSr[1][2]) < 1.0e-15
+    @test norm(spdata_f1[1] - spdata_r2[1]) < 1.0e-15
+    @test norm(spdata_f1[2] - spdata_r2[2]) < 1.0e-15
+    @test norm(spdata_f1[3] - spdata_r2[3]) < 1.0e-15
+    @test norm(spdata_f2[1] - spdata_r1[1]) < 1.0e-15
+    @test norm(spdata_f2[2] - spdata_r1[2]) < 1.0e-15
+    @test norm(spdata_f2[3] - spdata_r1[3]) < 1.0e-15
+    coeffs_f = hierarchical_coefficients(g,HSf)
+    coeffs_b = hierarchical_coefficients(gr,HSr)
+    @test abs(coeffs_f[1][1] - coeffs_b[2][1]) < 1.0e-15
+    @test abs(coeffs_f[1][2] - coeffs_b[2][2]) < 1.0e-15
+    @test abs(coeffs_f[2][1] - coeffs_b[1][1]) < 1.0e-15
+    @test abs(coeffs_f[2][2] - coeffs_b[1][2]) < 1.0e-15
 
     g = DiGraph(10)
     add_edge!(g,1,2)
@@ -87,19 +141,19 @@ using Test
     add_edge!(g,4,7)
     add_edge!(g,6,9)
     add_edge!(g,5,6)
-    HS = graphHierarchicalStructure(g)
+    HS = forward_hierarchical_structure(g)
     spdata = findnz(HS[3])
-    coeffs = graphHierarchicalCoefficients(HS)
+    coeffs = hierarchical_coefficients(g,HS)
     @test norm(HS[1] - [-4.24285714285708, -4.242857142857171, -4.242857142856976,  0.8999999999996752, -0.5285714285718714,  1.1857142857147973,  1.9000000000000274,  4.042857142857218,  2.185714285714233,  3.042857142857145]) < 1.0e-11
     @test norm(HS[2] - [1.0,1.0,1.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0]) < 1.0e-11
     @test norm(spdata[1] - [3,1,2,1,8,3,6,4,5,4,10,6,7,9]) < 1.0e-15
     @test norm(spdata[2] -[1,2,3,4,4,5,5,6,6,7,8,9,10,10]) < 1.0e-15
     @test norm(spdata[3] - [0,0,0,5.142857142856755,-3.142857142857543,3.7142857142851042,-1.7142857142866688,0.28571428571512214,1.7142857142866688,1,1,1,1.1428571428571175,0.8571428571429118]) < 1.0e-11
     @test abs(coeffs[1]-3.0/14.0) < 1.0e-11
-    @test abs(coeffs[2]-2.0092017518778356) < 1.0e-11
-    HSA = graphHierarchicalStructure(adjacency_matrix(g))
+    @test abs(coeffs[2]-1.936115253297853) < 1.0e-11
+    HSA = forward_hierarchical_structure(adjacency_matrix(g))
     spdataA = findnz(HSA[3])
-    coeffsA = graphHierarchicalCoefficients(HSA)
+    coeffsA = hierarchical_coefficients(g,HSA)
     @test norm(HSA[1] - HS[1]) < 1.0e-11
     @test norm(HSA[2] - HS[2]) < 1.0e-11
     @test norm(spdataA[1] - spdata[1]) < 1.0e-15
@@ -107,6 +161,31 @@ using Test
     @test norm(spdataA[3] - spdata[3]) < 1.0e-11
     @test abs(coeffsA[1] - coeffs[1]) < 1.0e-11
     @test abs(coeffsA[2] - coeffs[2]) < 1.0e-11
+    gr = SimpleDiGraph(transpose(adjacency_matrix(g)))
+    HSf = hierarchical_structure(g)
+    HSr = hierarchical_structure(gr)
+    spdata_f1 = findnz(HSf[1][3])
+    spdata_f2 = findnz(HSf[2][3])
+    coeffs_f = hierarchical_coefficients(g,HSf)
+    spdata_r1 = findnz(HSr[1][3])
+    spdata_r2 = findnz(HSr[2][3])
+    coeffs_r = hierarchical_coefficients(gr,HSr)
+    @test norm(HSf[1][1] - HSr[2][1]) < 1.0e-15
+    @test norm(HSf[2][1] - HSr[1][1]) < 1.0e-15
+    @test norm(HSf[1][2] - HSr[2][2]) < 1.0e-15
+    @test norm(HSf[2][2] - HSr[1][2]) < 1.0e-15
+    @test norm(spdata_f1[1] - spdata_r2[1]) < 1.0e-15
+    @test norm(spdata_f1[2] - spdata_r2[2]) < 1.0e-15
+    @test norm(spdata_f1[3] - spdata_r2[3]) < 1.0e-15
+    @test norm(spdata_f2[1] - spdata_r1[1]) < 1.0e-15
+    @test norm(spdata_f2[2] - spdata_r1[2]) < 1.0e-15
+    @test norm(spdata_f2[3] - spdata_r1[3]) < 1.0e-15
+    coeffs_f = hierarchical_coefficients(g,HSf)
+    coeffs_b = hierarchical_coefficients(gr,HSr)
+    @test abs(coeffs_f[1][1] - coeffs_b[2][1]) < 1.0e-15
+    @test abs(coeffs_f[1][2] - coeffs_b[2][2]) < 1.0e-15
+    @test abs(coeffs_f[2][1] - coeffs_b[1][1]) < 1.0e-15
+    @test abs(coeffs_f[2][2] - coeffs_b[1][2]) < 1.0e-15
 
     g = DiGraph(6)
     add_edge!(g,1,2)
@@ -115,9 +194,9 @@ using Test
     add_edge!(g,4,5)
     add_edge!(g,5,6)
     add_edge!(g,6,1)
-    HS = graphHierarchicalStructure(g)
+    HS = forward_hierarchical_structure(g)
     spdata = findnz(HS[3])
-    coeffs = graphHierarchicalCoefficients(HS)
+    coeffs = hierarchical_coefficients(g,HS)
     @test norm(HS[1]) < 1.0e-11
     @test norm(HS[2] - [1.0,1.0,1.0,1.0,1.0,1.0]) < 1.0e-11
     @test norm(spdata[1] - [6,1,2,3,4,5]) < 1.0e-15
@@ -125,9 +204,9 @@ using Test
     @test norm(spdata[3]) < 1.0e-11
     @test abs(coeffs[1]-1.0) < 1.0e-11
     @test abs(coeffs[2]-0.0) < 1.0e-11
-    HSA = graphHierarchicalStructure(adjacency_matrix(g))
+    HSA = forward_hierarchical_structure(adjacency_matrix(g))
     spdataA = findnz(HSA[3])
-    coeffsA = graphHierarchicalCoefficients(HSA)
+    coeffsA = hierarchical_coefficients(g,HSA)
     @test norm(HSA[1] - HS[1]) < 1.0e-11
     @test norm(HSA[2] - HS[2]) < 1.0e-11
     @test norm(spdataA[1] - spdata[1]) < 1.0e-15
@@ -135,6 +214,31 @@ using Test
     @test norm(spdataA[3] - spdata[3]) < 1.0e-11
     @test abs(coeffsA[1] - coeffs[1]) < 1.0e-11
     @test abs(coeffsA[2] - coeffs[2]) < 1.0e-11
+    gr = SimpleDiGraph(transpose(adjacency_matrix(g)))
+    HSf = hierarchical_structure(g)
+    HSr = hierarchical_structure(gr)
+    spdata_f1 = findnz(HSf[1][3])
+    spdata_f2 = findnz(HSf[2][3])
+    coeffs_f = hierarchical_coefficients(g,HSf)
+    spdata_r1 = findnz(HSr[1][3])
+    spdata_r2 = findnz(HSr[2][3])
+    coeffs_r = hierarchical_coefficients(gr,HSr)
+    @test norm(HSf[1][1] - HSr[2][1]) < 1.0e-15
+    @test norm(HSf[2][1] - HSr[1][1]) < 1.0e-15
+    @test norm(HSf[1][2] - HSr[2][2]) < 1.0e-15
+    @test norm(HSf[2][2] - HSr[1][2]) < 1.0e-15
+    @test norm(spdata_f1[1] - spdata_r2[1]) < 1.0e-15
+    @test norm(spdata_f1[2] - spdata_r2[2]) < 1.0e-15
+    @test norm(spdata_f1[3] - spdata_r2[3]) < 1.0e-15
+    @test norm(spdata_f2[1] - spdata_r1[1]) < 1.0e-15
+    @test norm(spdata_f2[2] - spdata_r1[2]) < 1.0e-15
+    @test norm(spdata_f2[3] - spdata_r1[3]) < 1.0e-15
+    coeffs_f = hierarchical_coefficients(g,HSf)
+    coeffs_b = hierarchical_coefficients(gr,HSr)
+    @test abs(coeffs_f[1][1] - coeffs_b[2][1]) < 1.0e-15
+    @test abs(coeffs_f[1][2] - coeffs_b[2][2]) < 1.0e-15
+    @test abs(coeffs_f[2][1] - coeffs_b[1][1]) < 1.0e-15
+    @test abs(coeffs_f[2][2] - coeffs_b[1][2]) < 1.0e-15
 
     g = DiGraph(4)
     add_edge!(g,1,2)
@@ -143,19 +247,19 @@ using Test
     add_edge!(g,2,3)
     add_edge!(g,3,4)
     add_edge!(g,4,1)
-    HS = graphHierarchicalStructure(g)
+    HS = forward_hierarchical_structure(g)
     spdata = findnz(HS[3])
-    coeffs = graphHierarchicalCoefficients(HS)
+    coeffs = hierarchical_coefficients(g,HS)
     @test norm(HS[1] - [-(5.0/8.0), -(1.0/8.0), 3.0/8.0, 3.0/8.0]) < 1.0e-15
     @test norm(HS[2] - [2.0,0.5,0.25,0.5]) < 1.0e-15
     @test norm(spdata[1] - [4,1,1,2,1,3]) < 1.0e-15
     @test norm(spdata[2] - [1,2,3,3,4,4]) < 1.0e-15
     @test norm(spdata[3] -[-1.0,0.5,1.0,0.5,1.0,0.0]) < 1.0e-15
     @test abs(coeffs[1] - 2.0/3.0) < 1.0e-15
-    @test abs(coeffs[2] - 0.7527726527090812) < 1.0e-15
-    HSA = graphHierarchicalStructure(adjacency_matrix(g))
+    @test abs(coeffs[2] - 0.687184270936277) < 1.0e-15
+    HSA = forward_hierarchical_structure(adjacency_matrix(g))
     spdataA = findnz(HSA[3])
-    coeffsA = graphHierarchicalCoefficients(HSA)
+    coeffsA = hierarchical_coefficients(g,HSA)
     @test norm(HSA[1] - HS[1]) < 1.0e-15
     @test norm(HSA[2] - HS[2]) < 1.0e-15
     @test norm(spdataA[1] - spdata[1]) < 1.0e-15
@@ -163,6 +267,31 @@ using Test
     @test norm(spdataA[3] - spdata[3]) < 1.0e-15
     @test abs(coeffsA[1] - coeffs[1]) < 1.0e-15
     @test abs(coeffsA[2] - coeffs[2]) < 1.0e-15
+    gr = SimpleDiGraph(transpose(adjacency_matrix(g)))
+    HSf = hierarchical_structure(g)
+    HSr = hierarchical_structure(gr)
+    spdata_f1 = findnz(HSf[1][3])
+    spdata_f2 = findnz(HSf[2][3])
+    coeffs_f = hierarchical_coefficients(g,HSf)
+    spdata_r1 = findnz(HSr[1][3])
+    spdata_r2 = findnz(HSr[2][3])
+    coeffs_r = hierarchical_coefficients(gr,HSr)
+    @test norm(HSf[1][1] - HSr[2][1]) < 1.0e-15
+    @test norm(HSf[2][1] - HSr[1][1]) < 1.0e-15
+    @test norm(HSf[1][2] - HSr[2][2]) < 1.0e-15
+    @test norm(HSf[2][2] - HSr[1][2]) < 1.0e-15
+    @test norm(spdata_f1[1] - spdata_r2[1]) < 1.0e-15
+    @test norm(spdata_f1[2] - spdata_r2[2]) < 1.0e-15
+    @test norm(spdata_f1[3] - spdata_r2[3]) < 1.0e-15
+    @test norm(spdata_f2[1] - spdata_r1[1]) < 1.0e-15
+    @test norm(spdata_f2[2] - spdata_r1[2]) < 1.0e-15
+    @test norm(spdata_f2[3] - spdata_r1[3]) < 1.0e-15
+    coeffs_f = hierarchical_coefficients(g,HSf)
+    coeffs_b = hierarchical_coefficients(gr,HSr)
+    @test abs(coeffs_f[1][1] - coeffs_b[2][1]) < 1.0e-15
+    @test abs(coeffs_f[1][2] - coeffs_b[2][2]) < 1.0e-15
+    @test abs(coeffs_f[2][1] - coeffs_b[1][1]) < 1.0e-15
+    @test abs(coeffs_f[2][2] - coeffs_b[1][2]) < 1.0e-15
 
     g = Graph(6)
     add_edge!(g,1,2)
@@ -172,19 +301,19 @@ using Test
     add_edge!(g,5,6)
     add_edge!(g,2,4)
     add_edge!(g,3,5)
-    HS = graphHierarchicalStructure(g)
+    HS = forward_hierarchical_structure(g)
     spdata = findnz(HS[3])
-    coeffs = graphHierarchicalCoefficients(HS)
+    coeffs = hierarchical_coefficients(g,HS)
     @test norm(HS[1] - [-1.0, 1.0/3.0, 2.0/3.0, 2.0/3.0, 1.0/3.0, -1.0]) < 1.0e-15
     @test norm(HS[2] - [7.0/3.0, 7.0/9.0, 7.0/9.0, 7.0/9.0, 7.0/9.0, 7.0/3.0]) < 1.0e-15
     @test norm(spdata[1] - [2,1,3,4,2,4,5,2,3,5,3,4,6,5]) < 1.0e-15
     @test norm(spdata[2] - [1,2,2,2,3,3,3,4,4,4,5,5,5,6]) < 1.0e-15
     @test norm(spdata[3] -[-4.0/3.0,4.0/3.0,-1.0/3.0,-1.0/3.0,1.0/3.0,0.0,1.0/3.0,1.0/3.0,0.0,1.0/3.0,-1.0/3.0,-1.0/3.0,4.0/3.0,-4.0/3.0]) < 1.0e-15
     @test abs(coeffs[1] - 1.0) < 1.0e-15
-    @test abs(coeffs[2] - 0.7844645405527362) < 1.0e-15
-    HSA = graphHierarchicalStructure(adjacency_matrix(g))
+    @test abs(coeffs[2] - 0.7559289460184544) < 1.0e-15
+    HSA = forward_hierarchical_structure(adjacency_matrix(g))
     spdataA = findnz(HSA[3])
-    coeffsA = graphHierarchicalCoefficients(HSA)
+    coeffsA = hierarchical_coefficients(g,HSA)
     @test norm(HSA[1] - HS[1]) < 1.0e-15
     @test norm(HSA[2] - HS[2]) < 1.0e-15
     @test norm(spdataA[1] - spdata[1]) < 1.0e-15
