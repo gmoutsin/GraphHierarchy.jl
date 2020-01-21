735f253b5f050b2260d5d51e53d61440a8499cdb
Giannis Moutsinas <giannis.moutsinas@gmail.com>
2020-01-21 16:23:34 +0000

    fixed weighted graphs

--- src/GraphHierarchy.jl
+++ src/GraphHierarchy.jl
@@ -11,7 +11,24 @@ using Statistics
 export forward_hierarchical_structure
 export backward_hierarchical_structure
 export hierarchical_structure
+export forward_hierarchical_coefficients
+export backward_hierarchical_coefficients
 export hierarchical_coefficients
+
+
+
+function weightedMeanStd(v::Array{T,1} where T <: AbstractFloat, w::Array{T,1} where T <: AbstractFloat)
+    difftype = typeof(v[1])
+    if difftype == typeof(w[1])
+        av0 = mean(v, StatsBase.weights(w))
+        return ( 1 - av0 ,std( v, StatsBase.weights(w), mean=av0, corrected=false))
+    else
+        ww = [ convert( difftype, c) for c in w ]
+        av = mean(v, StatsBase.weights(ww))
+        return ( 1 - av ,std( v, StatsBase.weights(ww), mean=av, corrected=false))
+    end
+end
+
 
 function calculatingStructureAssumingTransposedMatrix(A::SparseMatrixCSC{T1,T2} where T1 <: Number where T2 <: Int)
     d = vec(sum(A, dims=2))
@@ -24,6 +41,7 @@ function calculatingStructureAssumingTransposedMatrix(A::SparseMatrixCSC{T1,T2} 
     ICs = [d[i] == 0 ? 1.0 : 1.0 - sum(HDs[:,i])/d[i] for i in 1:length(d)]
     return (HLs, ICs, HDs )
 end
+
 
 function calculatingStructureAssumingTransposedMatrixBig(A::SparseMatrixCSC{T1,T2} where T1 <: Number where T2 <: Int)
     (x, y, v) = findnz(A)
@@ -41,6 +59,7 @@ function calculatingStructureAssumingTransposedMatrixBig(A::SparseMatrixCSC{T1,T
     return (HLs, ICs, HDs )
 end
 
+
 function calculatingStructureAssumingTransposedWeightedMatrix(A::SparseMatrixCSC{T1,T2} where T1 <: Number where T2 <: Int)
     (x, y, v) = findnz(A)
     d = vec(sum(A, dims=2))
@@ -52,6 +71,7 @@ function calculatingStructureAssumingTransposedWeightedMatrix(A::SparseMatrixCSC
     ICs = [d[i] > 0 ? 1.0 - A[i,:]'*HDs[:,i]/d[i] : 1.0 for i in 1:length(d)]
     return (HLs, ICs, sparse(HDs) )
 end
+
 
 function calculatingStructureAssumingTransposedWeightedMatrixBig(A::SparseMatrixCSC{T1,T2} where T1 <: Number where T2 <: Int)
     (x, y, v) = findnz(A)
@@ -70,6 +90,7 @@ function calculatingStructureAssumingTransposedWeightedMatrixBig(A::SparseMatrix
     return (HLs, ICs, HDs )
 end
 
+
 function forward_hierarchical_structure(g::SimpleDiGraph{T} where T <: Int; big::Bool=false)
     if big
         return calculatingStructureAssumingTransposedMatrixBig(sparse(transpose(adjacency_matrix(g))))
@@ -77,6 +98,7 @@ function forward_hierarchical_structure(g::SimpleDiGraph{T} where T <: Int; big:
         return calculatingStructureAssumingTransposedMatrix(sparse(transpose(adjacency_matrix(g))))
     end
 end
+
 
 function backward_hierarchical_structure(g::SimpleDiGraph{T} where T <: Int; big::Bool=false)
     if big
@@ -86,9 +108,11 @@ function backward_hierarchical_structure(g::SimpleDiGraph{T} where T <: Int; big
     end
 end
 
+
 function hierarchical_structure(g::SimpleDiGraph{T} where T <: Int; big::Bool=false)
     return (forward_hierarchical_structure(g, big = big),backward_hierarchical_structure(g, big = big))
 end
+
 
 function forward_hierarchical_structure(g::SimpleWeightedDiGraph{T1,T2} where T1 <: Int where T2 <: Number; big::Bool=false, weighted::Bool=true)
     if weighted
@@ -106,6 +130,7 @@ function forward_hierarchical_structure(g::SimpleWeightedDiGraph{T1,T2} where T1
     end
 end
 
+
 function backward_hierarchical_structure(g::SimpleWeightedDiGraph{T1,T2} where T1 <: Int where T2 <: Number; big::Bool=false, weighted::Bool=true)
     if weighted
         if big
@@ -122,9 +147,11 @@ function backward_hierarchical_structure(g::SimpleWeightedDiGraph{T1,T2} where T
     end
 end
 
+
 function hierarchical_structure(g::SimpleWeightedDiGraph{T1,T2} where T1 <: Int where T2 <: Number; big::Bool=false, weighted::Bool=true)
     return (forward_hierarchical_structure(g, big = big, weighted = weighted),backward_hierarchical_structure(g, big = big, weighted = weighted))
 end
+
 
 function forward_hierarchical_structure(g::SimpleGraph{T} where T <: Int; big::Bool=false)
     if big
@@ -134,13 +161,16 @@ function forward_hierarchical_structure(g::SimpleGraph{T} where T <: Int; big::B
     end
 end
 
+
 function backward_hierarchical_structure(g::SimpleGraph{T} where T <: Int; big::Bool=false)
     forward_hierarchical_structure(g, big = big)
 end
 
+
 function hierarchical_structure(g::SimpleGraph{T} where T <: Int; big::Bool=false)
     return forward_hierarchical_structure(g, big = big)
 end
+
 
 function forward_hierarchical_structure(g::SimpleWeightedGraph{T1,T2} where T1 <: Int where T2 <: Number; big::Bool=false, weighted::Bool=true)
     if weighted
@@ -158,13 +188,16 @@ function forward_hierarchical_structure(g::SimpleWeightedGraph{T1,T2} where T1 <
     end
 end
 
+
 function backward_hierarchical_structure(g::SimpleWeightedGraph{T1,T2} where T1 <: Int where T2 <: Number; big::Bool=false, weighted::Bool=true)
     return forward_hierarchical_structure(g, big = big, weighted = weighted)
 end
 
+
 function hierarchical_structure(g::SimpleWeightedGraph{T1,T2} where T1 <: Int where T2 <: Number; big::Bool=false, weighted::Bool=true)
     return forward_hierarchical_structure(g, big = big, weighted = weighted)
 end
+
 
 function forward_hierarchical_structure(A::SparseMatrixCSC{T1,T2} where T1 <: Number where T2 <: Int; big::Bool=false)
     if big
@@ -174,6 +207,7 @@ function forward_hierarchical_structure(A::SparseMatrixCSC{T1,T2} where T1 <: Nu
     end
 end
 
+
 function backward_hierarchical_structure(A::SparseMatrixCSC{T1,T2} where T1 <: Number where T2 <: Int; big::Bool=false)
     if big
         return calculatingStructureAssumingTransposedMatrixBig(A)
@@ -182,9 +216,11 @@ function backward_hierarchical_structure(A::SparseMatrixCSC{T1,T2} where T1 <: N
     end
 end
 
+
 function hierarchical_structure(A::SparseMatrixCSC{T1,T2} where T1 <: Number where T2 <: Int; big::Bool=false)
     return ( forward_hierarchical_structure(A, big=big), backward_hierarchical_structure(A, big=big) )
 end
+
 
 function hierarchical_coefficients(g::Union{SimpleGraph{T},SimpleDiGraph{T}} where T <: Int, diffs::SparseMatrixCSC{T1,T2} where T1 <: AbstractFloat where T2 <: Int)
     diffarray = findnz(diffs)[3]
@@ -192,52 +228,97 @@ function hierarchical_coefficients(g::Union{SimpleGraph{T},SimpleDiGraph{T}} whe
     return (1 - av, std(diffarray, mean=av, corrected=false))
 end
 
-function hierarchical_coefficients(g::Union{SimpleWeightedGraph{T1,T2},SimpleWeightedDiGraph{T1,T2}} where T1 <: Int where T2 <: AbstractFloat, diffs::SparseMatrixCSC{T1,T2} where T1 <: AbstractFloat where T2 <: Int; weighted::Bool = true )
+
+function hierarchical_coefficients(g::Union{SimpleGraph{T},SimpleDiGraph{T}} where T <: Int, hstruc::Tuple{Array{T1,1},Array{T1,1},SparseMatrixCSC{T1,T2}} where T1 <: AbstractFloat where T2 <: Int )
+    return hierarchical_coefficients(g, hstruc[3])
+end
+
+
+function forward_hierarchical_coefficients(g::Union{SimpleGraph{T},SimpleDiGraph{T}} where T <: Int, hstruc::Tuple{Array{T1,1},Array{T1,1},SparseMatrixCSC{T1,T2}} where T1 <: AbstractFloat where T2 <: Int )
+    return hierarchical_coefficients(g, hstruc[3])
+end
+
+
+function backward_hierarchical_coefficients(g::Union{SimpleGraph{T},SimpleDiGraph{T}} where T <: Int, hstruc::Tuple{Array{T1,1},Array{T1,1},SparseMatrixCSC{T1,T2}} where T1 <: AbstractFloat where T2 <: Int )
+    return hierarchical_coefficients(g, hstruc[3])
+end
+
+
+function forward_hierarchical_coefficients(g::Union{SimpleGraph{T},SimpleDiGraph{T}} where T <: Int, hstruc::Tuple{Array{T1,1},Array{T1,1},SparseMatrixCSC{T1,T2}} where T1 <: AbstractFloat where T2 <: Int )
+    return hierarchical_coefficients(g, hstruc[3])
+end
+
+
+function backward_hierarchical_coefficients(g::Union{SimpleGraph{T},SimpleDiGraph{T}} where T <: Int, hstruc::Tuple{Array{T1,1},Array{T1,1},SparseMatrixCSC{T1,T2}} where T1 <: AbstractFloat where T2 <: Int )
+    return hierarchical_coefficients(g, hstruc[3])
+end
+
+
+function hierarchical_coefficients(g::Union{SimpleGraph{T},SimpleDiGraph{T}} where T <: Int , hstruc::Tuple{Tuple{Array{T1,1},Array{T1,1},SparseMatrixCSC{T1,T2}},Tuple{Array{T1,1},Array{T1,1},SparseMatrixCSC{T1,T2}}} where T1 <: AbstractFloat where T2 <: Int )
+    return (hierarchical_coefficients(g, hstruc[1]),hierarchical_coefficients(g, hstruc[2]))
+end
+
+
+function forward_hierarchical_coefficients(g::Union{SimpleWeightedGraph{T1,T2},SimpleWeightedDiGraph{T1,T2}} where T1 <: Int where T2 <: AbstractFloat, diffs::SparseMatrixCSC{T1,T2} where T1 <: AbstractFloat where T2 <: Int; weighted::Bool = true )
     if weighted
         W0 = sparse(LightGraphs.weights(g))
         (xw,yw,vw) = findnz(W0)
         (x,y,v) = findnz(diffs)
 
         if sum(map(abs,x - xw)) + sum(map(abs,y - yw)) > 0
-            W0 = sparse(transpose(LightGraphs.weights(g)))
-            (xw,yw,vw) = findnz(W0)
+            error("forward hierarchical differences do not match the weighted adjacency matrix:\nDifferences:", diffs, "\nWeighted Adjacency Matrix:", W0)
         end
 
-        if sum(map(abs,x - xw)) + sum(map(abs,y - yw)) > 0
-            error("hierarchical differences do not match the weighted adjacency matrix:\n", diffs, "\n", W0)
-        end
-
-        w0 = [ W0[x[i],y[i]] for i in 1:length(x)]
-        difftype = typeof(v[1])
-        if difftype == typeof(w0[1])
-            av0 = mean(v, StatsBase.weights(w0))
-            return ( 1 - av0 ,std( v, StatsBase.weights(w0), mean=av0, corrected=false))
-        else
-            w = [ convert( difftype, c) for c in w0 ]
-            av = mean(v, StatsBase.weights(w))
-            return ( 1 - av ,std( v, StatsBase.weights(w), mean=av, corrected=false))
-        end
+        return weightedMeanStd(v,vw)
     else
         v = findnz(diffs)[3]
         av = mean(v)
-        return (1 - av, std(v, mean=av))
+        return (1 - av, std(v, mean=av, corrected=false))
     end
 end
 
-function hierarchical_coefficients(g::Union{SimpleGraph{T},SimpleDiGraph{T}} where T <: Int, hstruc::Tuple{Array{T1,1},Array{T1,1},SparseMatrixCSC{T1,T2}} where T1 <: AbstractFloat where T2 <: Int )
-    return hierarchical_coefficients(g, hstruc[3])
+
+function backward_hierarchical_coefficients(g::Union{SimpleWeightedGraph{T1,T2},SimpleWeightedDiGraph{T1,T2}} where T1 <: Int where T2 <: AbstractFloat, diffs::SparseMatrixCSC{T1,T2} where T1 <: AbstractFloat where T2 <: Int; weighted::Bool = true )
+    if weighted
+        W0 = sparse(transpose(LightGraphs.weights(g)))
+        (xw,yw,vw) = findnz(W0)
+        (x,y,v) = findnz(diffs)
+
+        if sum(map(abs,x - xw)) + sum(map(abs,y - yw)) > 0
+            error("backward hierarchical differences do not match the weighted adjacency matrix:\nDifferences:", diffs, "\nWeighted Adjacency Matrix:", W0 )
+        end
+
+        return weightedMeanStd(v,vw)
+    else
+        v = findnz(diffs)[3]
+        av = mean(v)
+        return (1 - av, std(v, mean=av, corrected=false))
+    end
 end
+
+function hierarchical_coefficients(g::Union{SimpleWeightedGraph{T1,T2},SimpleWeightedDiGraph{T1,T2}} where T1 <: Int where T2 <: AbstractFloat, diffs::SparseMatrixCSC{T1,T2} where T1 <: AbstractFloat where T2 <: Int; weighted::Bool = true )
+    return forward_hierarchical_coefficients(g, diffs, weighted=weighted)
+end
+
 
 function hierarchical_coefficients(g::Union{SimpleWeightedGraph{T1,T2},SimpleWeightedDiGraph{T1,T2}} where T1 <: Int  where T2 <: AbstractFloat, hstruc::Tuple{Array{T1,1},Array{T1,1},SparseMatrixCSC{T1,T2}} where T1 <: AbstractFloat where T2 <: Int;  weighted::Bool = true  )
-    return hierarchical_coefficients(g, hstruc[3], weighted=weighted)
+    return forward_hierarchical_coefficients(g, hstruc[3], weighted=weighted)
 end
 
-function hierarchical_coefficients(g::Union{SimpleGraph{T},SimpleDiGraph{T}} where T <: Int , hstruc::Tuple{Tuple{Array{T1,1},Array{T1,1},SparseMatrixCSC{T1,T2}},Tuple{Array{T1,1},Array{T1,1},SparseMatrixCSC{T1,T2}}} where T1 <: AbstractFloat where T2 <: Int )
-    return (hierarchical_coefficients(g, hstruc[1]),hierarchical_coefficients(g, hstruc[2]))
+
+function forward_hierarchical_coefficients(g::Union{SimpleWeightedGraph{T1,T2},SimpleWeightedDiGraph{T1,T2}} where T1 <: Int  where T2 <: AbstractFloat, hstruc::Tuple{Array{T1,1},Array{T1,1},SparseMatrixCSC{T1,T2}} where T1 <: AbstractFloat where T2 <: Int;  weighted::Bool = true  )
+    return forward_hierarchical_coefficients(g, hstruc[3], weighted=weighted)
 end
+
+
+function backward_hierarchical_coefficients(g::Union{SimpleWeightedGraph{T1,T2},SimpleWeightedDiGraph{T1,T2}} where T1 <: Int  where T2 <: AbstractFloat, hstruc::Tuple{Array{T1,1},Array{T1,1},SparseMatrixCSC{T1,T2}} where T1 <: AbstractFloat where T2 <: Int;  weighted::Bool = true  )
+    return backward_hierarchical_coefficients(g, hstruc[3], weighted=weighted)
+end
+
 
 function hierarchical_coefficients(g::Union{SimpleWeightedGraph{T1,T2},SimpleWeightedDiGraph{T1,T2}} where T1 <: Int  where T2 <: AbstractFloat , hstruc::Tuple{Tuple{Array{T1,1},Array{T1,1},SparseMatrixCSC{T1,T2}},Tuple{Array{T1,1},Array{T1,1},SparseMatrixCSC{T1,T2}}} where T1 <: AbstractFloat where T2 <: Int; weighted::Bool = true  )
-    return (hierarchical_coefficients(g, hstruc[1], weighted=weighted),hierarchical_coefficients(g, hstruc[2], weighted=weighted))
+    return (forward_hierarchical_coefficients(g, hstruc[1], weighted=weighted),backward_hierarchical_coefficients(g, hstruc[2], weighted=weighted))
 end
+
 
 end # module
